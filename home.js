41d182e60c6a72a2a82431dcd32a444f747fe1ae
Elliott Hansen <elliott@redvw.com>
2023-07-13 11:49:41 -0400

    changes

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
 
 const track = document.getElementById("scrollTrack");
-
+const scrollArrow = document.getElementById("scrollArrow");
+const bigImage = document.getElementById("bigImage");
 let amountScrolled = 0;
+let firstScroll = false;
 
 
 function isInViewport(element) {
@@ -15,19 +17,30 @@ function isInViewport(element) {
 }
 
 document.addEventListener('wheel', (event) => {
-    if (amountScrolled <= 0 && amountScrolled > -81) {
+    if (amountScrolled <= 0 && amountScrolled > -161) {
         if (amountScrolled - event.deltaY / 102 >= 1) {
             return;
         }
-        if (amountScrolled - event.deltaY / 102 === -81 ) {
+        if (amountScrolled - event.deltaY / 102 === -161 ) {
             return;
         }
+        if(!firstScroll) {
+            scrollArrow.animate( {
+                opacity: '0%'
+            } , {easing: 'ease-out', duration: 400, fill: 'forwards'});
+            firstScroll = true;
+        }
         amountScrolled -= (event.deltaY / 102) * 8;
-        track.animate( { transform: 'translateY('+amountScrolled+'%)' }, {easing: 'ease-in-out',duration: 800, fill: 'forwards'} );
+        console.log(amountScrolled);
+        track.animate( { transform: 'translateY('+amountScrolled*0.6+'%)' }, {easing: 'ease-in-out',duration: 800, fill: 'forwards'} );
 
         for (const img of track.getElementsByClassName("Image")) {
             img.animate({ transform: 'translateY('+(-amountScrolled)+'%)' }, {easing: 'ease-in-out',duration: 800, fill: 'forwards'});
             // img.animate({ objectPosition: 'center '+(-amountScrolled)/3+"%"}, {easing: 'ease-in-out',duration: 800, fill: 'forwards'});  
+        }
+
+        for (const img of bigImage.getElementsByClassName("StaticImage")) {
+            img.animate({ objectPosition: 'center '+(-amountScrolled)/3.3+"%"}, {easing: 'ease-out',duration: 800, fill: 'forwards'});  
         }
     }
 });
