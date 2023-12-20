ce9f5179ae3ef3fd443761f0ee14768a592a1ccb
elliott-hansen <elliott@redvw.com>
2023-12-19 19:01:10 -0500

    rework 3

--- main.js
+++ main.js
@@ -1,56 +1,31 @@
-console.log("Loaded JS");
-let amountScrolled = 0;
-const SCROLL_MULT = 105;
-const MAX_TOP = 0;
-var MAX_BOTTOM = 800;
-const scrollables = document.getElementsByClassName("scrollable");
-
-
-function setScroll(dir) {
-    if (dir > 0) {
-        if (amountScrolled >= MAX_BOTTOM) {
-            return
-        }
-        amountScrolled += (1 * SCROLL_MULT);
-    }
-    else if (dir < 0) {
-        if (amountScrolled <= 0) {
-            return;
-        }
-        amountScrolled -= (1 * SCROLL_MULT);
-    }
-}
-
-function moveItem(i, delay) {
-    i.animate(
-        {transform: 'translateY('+(-amountScrolled)+'px)'},
-        {easing: 'ease-in-out', duration: parseInt(delay), fill: 'forwards'}
-    )
-}
-
-document.addEventListener("wheel", e => {
-    setScroll(e.deltaY);
-    for (var i = 0; i < scrollables.length; i++) {
-        moveItem(scrollables[i], scrollables[i].getAttribute('delay'));
-    }
-}) 
-
-
-// THEMES
-
-
 const html = document.querySelector('html');
-html.dataset.theme = 'darkmode'
-var color_scheme = 'dark';
+console.log(localStorage.getItem('colorscheme'));
+
+try {
+    var color_scheme = localStorage.getItem("colorscheme");
+    if (color_scheme == 'dark') {
+        html.dataset.theme = 'darkmode'
+    }
+    else {
+        html.dataset.theme = 'lightmode'
+    }
+}
+catch {
+    console.log('fail');
+}
 
 function darkMode() {
+    localStorage.setItem("colorscheme", 'darkmode');
     html.dataset.theme = 'darkmode'
     color_scheme = 'dark';
+    console.log(localStorage.getItem("colorscheme"));
 }
 
 function lightMode() {
+    localStorage.setItem("colorscheme", 'lightmode');
     html.dataset.theme = 'lightmode'
     color_scheme = 'light';
+    console.log(localStorage.getItem("colorscheme"));
 }
 
 function toggleColors() {
